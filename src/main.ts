bfbdcdc8d5f0cab987b9d104cb0cd65a90ba2fac
Taku Nakagawa <r.taaaaabo@gmail.com>
2020-08-03 00:31:25 +0900

    Modify main file for amplify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+// Amplify Config
+import Amplify, { Auth } from 'aws-amplify';
+import PubSub from '@aws-amplify/pubsub';
+import API from '@aws-amplify/api';
+import awsmobile from './aws-exports';
+
+Amplify.configure(awsmobile);
+API.configure(awsmobile);
+PubSub.configure(awsmobile);
+Auth.configure(awsmobile);
 
 if (environment.production) {
   enableProdMode();
