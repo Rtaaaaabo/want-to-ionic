83270765a29ec6b21341a600d68065ded001fb59
Taku Nakagawa <r.taaaaabo@gmail.com>
2020-08-05 00:15:44 +0900

    create config file for jest

---
+++ src/jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  globals: {
+    "ts-jest": {
+      tsConfig: "./tsconfig.spec.json",
+      stringifyContentPathRegex: "\\.html$",
+      astTransformers: ["jest-preset-angular/InlineHtmlStripStylesTransformer"],
+    },
+  },
+  testMatch: ["**/*.spec.ts"],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  //   testEnvironment: "jest-environment-jsdom-thirteen",
+  moduleFileExtensions: ["ts", "html", "js", "json"],
+  moduleNameMapper: {
+    "^src/(.*)$": "./src/$1",
+    "^app/(.*)$": "./src/app/$1",
+    "^assets/(.*)$": "./src/assets/$1",
+    "^environments/(.*)$": "./src/environments/$1",
+  },
+  transformIgnorePatterns: ["node_modules/.*"],
+  // snapshotSerializers: [
+  //   "jest-preset-angular/AngularSnapshotSerializer.js",
+  //   "jest-preset-angular/HTMLCommentSerializer.js",
+  // ],
+};
