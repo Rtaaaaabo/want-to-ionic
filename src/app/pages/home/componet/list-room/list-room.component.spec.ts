b4f56ba09d5a539d820954eca1310972b11d75b9
Rtaaaaabo <r.taaaaabo@gmail.com>
2020-08-05 23:52:25 +0900

    Create listRoom component for HomePage

---
+++ src/app/pages/home/componet/list-room/list-room.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { ListRoomComponent } from './list-room.component';
+
+describe('ListRoomComponent', () => {
+  let component: ListRoomComponent;
+  let fixture: ComponentFixture<ListRoomComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ListRoomComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ListRoomComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
