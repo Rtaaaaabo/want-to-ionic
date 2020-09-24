069ac93c224dd1285546a1db896f537a13d023c6
Rtaaaaabo <r.taaaaabo@gmail.com>
2020-09-24 23:00:53 +0900

    Create template modal for edit profile ui

--- src/app/shared/component/modal/edit-profile-modal/edit-profile-modal.component.ts
+++ src/app/shared/component/modal/edit-profile-modal/edit-profile-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
@@ -8,16 +9,21 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class EditProfileModalComponent implements OnInit {
   editProfile = new FormGroup({
-    iconimage: new FormControl(''),
+    iconImage: new FormControl(''),
     name: new FormControl('', [Validators.required]),
-    nickname: new FormControl('', [Validators.required]),
     positionName: new FormControl(''),
     email: new FormControl('', [Validators.email]),
     tel: new FormControl(''),
   })
 
-  constructor() { }
+  constructor(
+    private modalCtrl: ModalController,
+  ) { }
 
   ngOnInit() { }
+
+  dismissModal() {
+    this.modalCtrl.dismiss();
+  }
 
 }
