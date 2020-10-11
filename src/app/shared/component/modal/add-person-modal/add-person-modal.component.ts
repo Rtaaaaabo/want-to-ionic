e37a7e64c34339a9a6be0c4c3214530fc2fa35ce
Rtaaaaabo <r.taaaaabo@gmail.com>
2020-10-11 23:14:13 +0900

    Modify add person to task

--- src/app/shared/component/modal/add-person-modal/add-person-modal.component.ts
+++ src/app/shared/component/modal/add-person-modal/add-person-modal.component.ts
@@ -9,12 +9,14 @@ import { ModalController } from '@ionic/angular';
 export class AddPersonModalComponent implements OnInit {
 
   members;
+  arraySelectedPersonId: Array<string>;
 
   constructor(
     private modalCtrl: ModalController,
   ) { }
 
   ngOnInit(): void {
+    this.arraySelectedPersonId = [];
     console.log(this.members);
   }
 
@@ -23,6 +25,23 @@ export class AddPersonModalComponent implements OnInit {
 
   dismissModal(): void {
     this.modalCtrl.dismiss();
+  }
+
+  addPersonToTask(): void {
+    console.log('Add person to task');
+  }
+
+  changeCheckUser(memberId: string): void {
+    const indexTargetId = this.arraySelectedPersonId.findIndex(item => item === memberId);
+    console.log(indexTargetId);
+    if (indexTargetId >= 0) {
+      console.log('Splice');
+      this.arraySelectedPersonId.splice(indexTargetId, 1);
+    } else {
+      console.log('Push');
+      this.arraySelectedPersonId.push(memberId);
+    }
+    console.log(this.arraySelectedPersonId);
   }
 
 }
