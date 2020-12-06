87421b92bc383b9fd96b5e597860bea8ab14ad36
Rtaaaaabo <r.taaaaabo@gmail.com>
2020-12-06 23:51:30 +0900

    Own-ServiceにAmplifyを追加する

--- src/app/pages/own-task/service/own-task.service.ts
+++ src/app/pages/own-task/service/own-task.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { from, Observable } from 'rxjs';
+import { AmplifyService } from 'src/app/shared/service/amplify.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OwnTaskService {
 
-  constructor() { }
+  constructor(
+    private amplifyService: AmplifyService,
+  ) { }
+
+  // FilterContentには UserIdを入れる
+  fetchListRoomGroup(filterContent): Observable<any> {
+    return from(this.amplifyService.ListRoomGroups(filterContent));
+  }
 }
