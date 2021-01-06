dd215ef8047d89efe1d84f79e2d311fb67bf5190
Rtaaaaabo <r.taaaaabo@gmail.com>
2021-01-07 01:10:44 +0900

    [Directive] FileDropDirectiveの作成

--- src/app/shared/directive/upload-file.directive.ts
+++ src/app/shared/directive/upload-file.directive.ts
@@ -1,9 +1,39 @@
-import { Directive } from '@angular/core';
+import { Directive, EventEmitter, HostListener, Output } from '@angular/core';
 
 @Directive({
   selector: '[appUploadFile]'
 })
 export class UploadFileDirective {
+
+  /**
+   * ファイルドロップ時のイベント
+   */
+  @Output()
+  public onFileDrop: EventEmitter<File[]> = new EventEmitter<File[]>();
+
+  /**
+   * ファイルが要素にドラッグされて重なった時のイベント
+   * ドラッグイベントを解除しておかないとドロップイベント時にブラウザがファイルを開く動作をしてしまう
+   * stopPropagation() は不要な様子
+   *
+   * @param event イベント
+   */
+  @HostListener('dragover', ['$event'])
+  public onDragOver(event: any): void {
+    event.preventDefault();
+  }
+
+  /**
+   * ファイルドロップ時のイベント
+   * 取得したファイルを引数に onFileDrop イベントを発火させる
+   * 
+   * @param event イベント
+   */
+  @HostListener('drop', ['$event'])
+  public onDrop(event: any): void {
+    event.preventDefault();
+    this.onFileDrop.emit(event.dataTransfer.files);
+  }
 
   constructor() { }
 
