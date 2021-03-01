1d54447948f94e50551bf688e4e97ff430c767f8
Rtaaaaabo <r.taaaaabo@gmail.com>
2021-03-01 23:53:13 +0900

    Component側の実装

--- src/app/pages/own-task/own-task.page.ts
+++ src/app/pages/own-task/own-task.page.ts
@@ -1,7 +1,8 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { LoadingController } from '@ionic/angular';
-import { concatMap, map } from 'rxjs/operators';
+import { LoadingController, AlertController, ToastController } from '@ionic/angular';
+import { from } from 'rxjs';
+import { concatMap, map, tap } from 'rxjs/operators';
 import { OwnTaskLogic } from './logic/own-task.logic';
 
 @Component({
@@ -11,18 +12,21 @@ import { OwnTaskLogic } from './logic/own-task.logic';
 })
 export class OwnTaskPage implements OnInit, AfterViewInit {
   ownTaskItems: Array<any>;
+  currentUserId: string;
 
   constructor(
     private router: Router,
     private loadingCtrl: LoadingController,
     private logic: OwnTaskLogic,
+    private alertCtrl: AlertController,
+    private readonly toastCtrl: ToastController,
   ) { }
 
   ngOnInit() { }
 
   ngAfterViewInit() {
     this.logic.fetchCurrentUser()
-      .pipe(map(result => result.sub))
+      .pipe(map(result => this.currentUserId = result.sub))
       .pipe(concatMap((currentUserId) => this.logic.getTaskChargeItems(currentUserId)))
       .pipe(concatMap(({ items }) => this.logic.setTaskPerRoom(items)))
       .pipe(concatMap((items) => this.logic.filterExceptDoneTask(items)))
@@ -46,4 +50,35 @@ export class OwnTaskPage implements OnInit, AfterViewInit {
     this.router.navigate(['task-detail', `${item.task.id}`, `active`]);
   }
 
+  async presentDoneTaskAlert(alertBody): Promise<void> {
+    const alert = await this.alertCtrl.create({
+      header: '完了にしますか？',
+      message: `${alertBody.title}を完了します。`,
+      buttons: [
+        {
+          text: 'キャンセル',
+          role: 'cancel',
+        },
+        {
+          text: 'OK',
+          role: 'ok',
+          handler: () => {
+            const presentToast = from(this.presentDoneToast());
+            this.logic.updateDoneTaskItem(alertBody, 10)
+              .pipe(concatMap(() => this.logic.getTaskChargeItems(this.currentUserId)))
+              .pipe(tap(() => presentToast))
+              .subscribe((data) => { this.ownTaskItems = data; })
+          }
+        }
+      ]
+    });
+  }
+
+  async presentDoneToast(): Promise<void> {
+    const toast = await this.toastCtrl.create({
+      message: 'おつかれさまでした',
+      duration: 5000,
+    });
+    toast.present();
+  }
 }
