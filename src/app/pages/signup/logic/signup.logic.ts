67fc4586cb1dcdc1cc4230ea851ae2b6c5a4429f
Rtaaaaabo <r.taaaaabo@gmail.com>
2021-04-04 14:32:38 +0900

    Signup logicのSpec周り

--- src/app/pages/signup/logic/signup.logic.ts
+++ src/app/pages/signup/logic/signup.logic.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { SessionService } from '../../../shared/service/session.service';
-import { AmplifyService } from '../../../shared/service/amplify.service';
-import { v4 as uuid } from 'uuid';
-import { Observable, from } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +9,6 @@ export class SignupLogic {
 
   constructor(
     private sessionService: SessionService,
-    private amplifyService: AmplifyService,
   ) { }
 
   entrySignupUser(value): Observable<any> {
@@ -25,25 +22,4 @@ export class SignupLogic {
     return this.sessionService.entryUserSignup(signupContent);
   }
 
-  /*
-  value = {
-    Session: null
-    authenticationFlowType: "USER_SRP_AUTH"
-    client: Client {endpoint: "https://cognito-idp.ap-northeast-1.amazonaws.com/", fetchOptions: {…}}
-    userDataKey: "CognitoIdentityServiceProvider.47bkbu7qf1aue8v7i0elq754tr.r.taaaaabo+dev03@gmail.com.userData"
-    username: "r.taaaaabo+dev03@gmail.com"
-  }
-  */
-  createUser(value, formValue, companyId): Observable<any> {
-    const isoNowDate = new Date().toISOString();
-    const createUser = {
-      id: `${isoNowDate}_${uuid()}`, // いつ作成されたUserであるのか判断できるようにした
-      email: formValue.email,
-      companyID: companyId,
-      username: formValue.username,
-      registered: false,
-      authority: 'member',
-    }
-    return from(this.amplifyService.CreateUser(createUser));
-  }
 }
