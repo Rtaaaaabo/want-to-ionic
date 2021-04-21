2510fdea29c9b3ae648ca95cfe3175033045b378
Rtaaaaabo <r.taaaaabo@gmail.com>
2021-04-21 23:58:02 +0900

    Functionの作成

--- amplify/backend/function/S3Trigger8b3c74c8/src/index.js
+++ amplify/backend/function/S3Trigger8b3c74c8/src/index.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line
 exports.handler = function (event, context) {
   console.log("Received S3 event:", JSON.stringify(event, null, 2));
-  // Get the object from the event and show its content type
   const bucket = event.Records[0].s3.bucket.name; //eslint-disable-line
   const key = event.Records[0].s3.object.key; //eslint-disable-line
   console.log(`Bucket: ${bucket}`, `Key: ${key}`);
