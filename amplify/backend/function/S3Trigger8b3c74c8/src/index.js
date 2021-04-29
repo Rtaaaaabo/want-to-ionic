6ea3df741cce7fbef6699750f7958b6f85dd8035
Rtaaaaabo <r.taaaaabo@gmail.com>
2021-04-30 00:00:01 +0900

    Amplify

--- amplify/backend/function/S3Trigger8b3c74c8/src/index.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line
-exports.handler = function (event, context) {
-  console.log("Received S3 event:", JSON.stringify(event, null, 2));
-  const bucket = event.Records[0].s3.bucket.name; //eslint-disable-line
-  const key = event.Records[0].s3.object.key; //eslint-disable-line
-  context.done(null, "Successfully processed S3 event"); // SUCCESS with message
-};
