0265d8121c86bf6df4f2f2a1bfb5abf47b84e287
Rtaaaaabo <r.taaaaabo@gmail.com>
2021-05-24 22:08:13 +0900

    Create Company componentの作成

--- src/app/pages/create-company/create-company.page.ts
+++ src/app/pages/create-company/create-company.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-create-company',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateCompanyPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly location: Location,
+  ) { }
 
   ngOnInit() {
+  }
+
+  goBackToSetting(): void {
+    this.location.back();
   }
 
 }
