1757aa441d09eacfe9a060da6403be94b587248e
Rtaaaaabo <r.taaaaabo@gmail.com>
2021-06-02 23:04:17 +0900

    verify Token apiの作成

--- amplify/backend/function/verifyToken/src/app.js
+++ amplify/backend/function/verifyToken/src/app.js
@@ -6,87 +6,73 @@ or in the "license" file accompanying this file. This file is distributed on an 
 See the License for the specific language governing permissions and limitations under the License.
 */
 
-
-
-
-var express = require('express')
-var bodyParser = require('body-parser')
-var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const AWS = require("aws-sdk");
+var express = require("express");
+var awsServerlessExpressMiddleware = require("aws-serverless-express/middleware");
+import { authenticator } from "otplib";
+require("dotenv").config();
 
 // declare a new express app
-var app = express()
-app.use(bodyParser.json())
-app.use(awsServerlessExpressMiddleware.eventContext())
+var app = express();
+app.use(express.json());
+app.use(awsServerlessExpressMiddleware.eventContext());
 
 // Enable CORS for all methods
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "*")
-  next()
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-
-/**********************
- * Example get method *
- **********************/
-
-app.get('/verify-otp', function(req, res) {
+app.get("/verify-otp", function (req, res) {
   // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
+  res.json({ success: "get call succeed!", url: req.url });
 });
 
-app.get('/verify-otp/*', function(req, res) {
+app.get("/verify-otp/generate", function (req, res) {
+  // One time passwordを生成する
+  const token = authenticator.generate(process.env.OTP_SECRET_DEV);
+  res.json({ success: "post call succeed!", opt: token });
+});
+
+app.post("/verify-otp", function (req, res) {
+  const token = req.body.otp; // OneTimePasswordを取得
+  try {
+    authenticator.check(token, process.env.OTP_SECRET_DEV); // 受け取ったtokenは正であるか確認
+    res.json({}); // 受け取ったTokenが正であれば
+  } catch (err) {
+    res.json({ error: err });
+  }
+});
+
+app.put("/verify-otp", function (req, res) {
   // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
+  res.json({ success: "put call succeed!", url: req.url, body: req.body });
 });
 
-/****************************
-* Example post method *
-****************************/
-
-app.post('/verify-otp', function(req, res) {
+app.put("/verify-otp/*", function (req, res) {
   // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+  res.json({ success: "put call succeed!", url: req.url, body: req.body });
 });
 
-app.post('/verify-otp/*', function(req, res) {
+app.delete("/verify-otp", function (req, res) {
   // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+  res.json({ success: "delete call succeed!", url: req.url });
 });
 
-/****************************
-* Example put method *
-****************************/
-
-app.put('/verify-otp', function(req, res) {
+app.delete("/verify-otp/*", function (req, res) {
   // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+  res.json({ success: "delete call succeed!", url: req.url });
 });
 
-app.put('/verify-otp/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
-});
-
-/****************************
-* Example delete method *
-****************************/
-
-app.delete('/verify-otp', function(req, res) {
-  // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
-});
-
-app.delete('/verify-otp/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
-});
-
-app.listen(3000, function() {
-    console.log("App started")
+app.listen(3000, function () {
+  console.log("App started");
 });
 
 // Export the app object. When executing the application local this does nothing. However,
 // to port it to AWS Lambda we will create a wrapper around that will load the app from
 // this file
-module.exports = app
+module.exports = app;
