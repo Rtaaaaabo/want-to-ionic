683978c43a0fffd51c6f9c063952e9bbd23cacae
Rtaaaaabo <r.taaaaabo@gmail.com>
2021-06-06 23:25:44 +0900

    Registration CompanyのComponentの作成

--- src/app/pages/main-registration-company/main-registration-company.page.ts
+++ src/app/pages/main-registration-company/main-registration-company.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-main-registration-company',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainRegistrationCompanyPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly alertCtrl: AlertController,
+  ) { }
 
   ngOnInit() {
+    this.presentCorrectToken();
   }
 
+  async presentCorrectToken(): Promise<void> {
+    const alert = await this.alertCtrl.create({
+      header: '確認いたしました',
+      message: '本登録を進めてください',
+      buttons: ['OK'],  // Alertを閉じるのみ
+    });
+    await alert.present();
+  }
+
+  async presentIncorrectToken(): Promise<void> {
+    const alert = await this.alertCtrl.create({
+      header: '不正です',
+      message: '正常な登録ができませんでした。正常な登録処理を行い、メール内容のリンクをクリックをクリックし登録作業を行ってください',
+      buttons: [
+        {
+          text: 'OK',
+          role: 'incorrect',
+          handler: () => {
+            console.log('LPに戻すでよいか?')
+          },
+        }
+      ],
+    });
+    await alert.present();
+  }
 }
