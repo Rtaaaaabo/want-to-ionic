5b0e96863d1713cdb7ee7bff025afac060862d7b
Rtaaaaabo <r.taaaaabo@gmail.com>
2021-07-12 23:50:36 +0900

    Dismiss modal

--- src/app/shared/component/modal/member-detail-modal/member-detail-modal.component.ts
+++ src/app/shared/component/modal/member-detail-modal/member-detail-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { CompanyMember } from 'src/app/pages/member-list/models/member-list.interface';
 
 @Component({
   selector: 'app-member-detail-modal',
@@ -6,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail-modal.component.scss'],
 })
 export class MemberDetailModalComponent implements OnInit {
+  @Input() detailUser: CompanyMember;
 
-  constructor() { }
+  constructor(
+    private readonly modalCtrl: ModalController,
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log(this.detailUser);
+  }
+
+  dismissModal(): void {
+    this.modalCtrl.dismiss();
+  }
 
 }
