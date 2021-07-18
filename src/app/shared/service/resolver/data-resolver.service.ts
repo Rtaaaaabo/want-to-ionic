80ef8ba89dbf8b7fafeda95eb5ed99ef19cb5e6e
Rtaaaaabo <r.taaaaabo@gmail.com>
2021-07-18 22:40:35 +0900

    dataResolverServiceの作成

--- src/app/shared/service/resolver/data-resolver.service.ts
+++ src/app/shared/service/resolver/data-resolver.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { DataLogic } from '../../logic/data/data.logic';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DataResolverService {
+export class DataResolverService implements Resolve<any> {
 
-  constructor() { }
+  constructor(private dataLogic: DataLogic) { }
+
+  resolve(route: ActivatedRouteSnapshot) {
+    let id = route.paramMap.get('id');
+    return this.dataLogic.getData(id);
+  }
 }
