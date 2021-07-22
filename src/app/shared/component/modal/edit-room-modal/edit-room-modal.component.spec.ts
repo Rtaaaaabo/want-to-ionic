cac93b18029e0a25d6aaf74a7c0c240c4fc173ee
Rtaaaaabo <r.taaaaabo@gmail.com>
2021-07-22 22:44:04 +0900

    Create EditRoomModalComponent

---
+++ src/app/shared/component/modal/edit-room-modal/edit-room-modal.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { EditRoomModalComponent } from './edit-room-modal.component';
+
+describe('EditRoomModalComponent', () => {
+  let component: EditRoomModalComponent;
+  let fixture: ComponentFixture<EditRoomModalComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EditRoomModalComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(EditRoomModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/component/modal/edit-room-modal/edit-room-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-room-modal',
+  templateUrl: './edit-room-modal.component.html',
+  styleUrls: ['./edit-room-modal.component.scss'],
+})
+export class EditRoomModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
